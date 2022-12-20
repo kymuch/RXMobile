8f753d56a7a9457283e5c6105f75a1b98459b38e
Pavel <pvlpeshko@gmail.com>
2022-12-20 21:12:21 +0100

    first commit

---
+++ lang.js
@@ -0,0 +1,37 @@
+const langArr = {
+    "unit" :  {
+        "pl": " innowacyjną firmą programistyczną, która rozwiązuje problemy użytkowników.",
+        "en": "an innovative software company that solves user problems. ",
+        "ua": "інноваційна компанія-розробник програмного забезпечення, яка вирішує проблеми користувачів.",
+    }, 
+    "chip": {
+        "pl": "Frontend to jedno z najpopularniejszych miejsc w sieci frontend deweloperzy są integralnymi członkami każdego zespołu, który zajmuje się tworzeniem stron internetowych, aplikacji internetowych lub dowolnych produktów w Internecie.",
+        "en": "Frontend-this is one of the most popular areas in Frontend neck developers are integral members of any team that develops websites, web applications or any products on the internet.",
+        "ua": "Frontend - це один з найбільш популярних напрямків в ШЕю Фронтенд розробники є невід'ємними учасниками будь-якої команди, яка займається розробкою веб-сайтів, веб-додатків або будь-яких продуктів в інтернеті.",
+    }, 
+    "memory": {
+        "pl": "Nasz Zespół / Nasza Praca / Technologie / Branże / Firma / Kontakt",
+        "en": "Our Team / Our Work / Technologies / Industries / Company / Contact Us",
+        "ua": "Наша команда / Hаша робота / Tехнології / Галузі / Kомпанія / Зв'яжіться з нами",
+    }, 
+    "memory-type": {
+        "pl": "Znajdź nas",
+        "en": "Find us",
+        "ua": "Знайти нас",
+    }, 
+    "courses": {
+        "pl": "Kontakt",
+        "en": "Contacts",
+        "ua": "Контакт",
+    }, 
+    "more": {
+        "pl": "Znajdź nas",
+        "en": "Find us",
+        "ua": "Знайти нас",
+    },
+    "product": {
+        'pl': "Kurs",
+        "en": "Courses",
+        "ua": "Курсы",
+    } 
+}
